aadd46e947e6659c63e6403f8c531940fe60775e
Artenlaclase <artenlaclase@gmail.com>
2024-04-15 16:11:36 -0400

    vota y muestra resultado

--- poo/index.js
+++ poo/index.js
@@ -3,27 +3,27 @@ votar encuesta
 contar votos 
 mostrar resultados */
 
-class Encuesta {   
-    constructor(question, options,answer) {
+class Encuesta {
+    constructor(question, options) {
         this.question = question;
         this.options = options;
-        this.answers = new Array(options,length).fill(0); //Inicio answers en 0
-    
+        this.answers = new Array(options.length).fill(0); //Inicio answers en 0
+
 
     }
 
-    mostrarQuestion () {
+    mostrarQuestion() {
         console.log(this.question);
         this.options.forEach((option, index) => {
-            console.log(`${index +1}. ${option}`);
+            console.log(`${index + 1}. ${option}`);
         });
     }
 
-    votar () {
+    votar() {
         alert("Luego de revisar la consola ");
-        let answer = prompt (" Ingrese el número de la opción que se desea votar: ");
+        let answer = prompt(" Ingrese el número de la opción que se desea votar: ");
         answer = parseInt(answer);
-        if (isNaN(answer) ||  answer < 1 ||  answer > this.options.length) {
+        if (isNaN(answer) || answer < 1 || answer > this.options.length) {
             console.log(" Opcion no validad. Debe ingresar un número entre 1 y " + this.options.length);
             return this.votar(); // volver  asolicitar respuesta 
 
@@ -34,13 +34,13 @@ class Encuesta {
 
     }
 
-    mostrarResult () {
+    mostrarResult() {
         console.log(" Resultado de la votación: ");
         this.options.forEach((option, index) => {
-        console.log (`${index +1}. ${option}: ${this.answers[index]} votos`);
+            console.log(`${index + 1}. ${option}: ${this.answers[index]} votos`);
         })
     }
-    realizarEncuesta(numVotantes){
+    realizarEncuesta(numVotantes) {
         for (let i = 0; i < numVotantes; i++) {
             console.log(`\nPersona ${i + 1}`);
             this.mostrarQuestion();
@@ -49,31 +49,31 @@ class Encuesta {
         }
     }
 
-} 
+}
 
 // Creando preguntas y alternativas
+
 let votantes = parseInt(prompt(" Ingrese el número de votantes ")); //encuestar a más personas 
+let n = parseInt(prompt(" Ingrese el número de preguntas ")); //Numero de preguntas 
 let questions = []
-let n = 2;
 
 for (let i = 0; i < n; i++) {
-    let question = prompt (`Ingrese la pregunta ${i +1}:`);
-    let numOptions = parseInt( prompt ("Ingrese el número de alternativas:  "));
+    let question = prompt(`Ingrese la pregunta ${i + 1}:`);
+    let numOptions = parseInt(prompt("Ingrese el número de alternativas:  "));
     // buscar forma de cautelar que el numero de alternativas sea 2 y maximo a
 
     let options = [];
     for (let j = 0; j < numOptions; j++) {
-        options.push (prompt(`Ingrese la alternativa ${j + 1}:` ));
-
+        options.push(prompt(`Ingrese la alternativa ${j + 1}:`));
     }
-    questions.push (new Encuesta (question, options));
+    questions.push(new Encuesta(question, options));
 
 }
 
 
 // Votar
 
-questions. forEach( (question, index) => {
+questions.forEach((question, index) => {
     console.log(`\nPregunta ${index + 1}`);
     //document.write(`\nPregunta ${index + 1}`);
     question.realizarEncuesta(votantes);
@@ -82,7 +82,7 @@ questions. forEach( (question, index) => {
 
 // Mostrar resultados 
 
-questions. forEach( (question, index) => {
+questions.forEach((question, index) => {
     console.log(`\nResultado de la pregunta ${index + 1}:`);
     //document.write(`\nResultado de la pregunta ${index + 1}:`);
     question.mostrarResult();
